3a81937c7b8f03fae1ac8dc7e57a6572ab4f3d07
kbledesign <kbledesign@gmail.com>
2023-10-15 10:12:09 -0400

    Updated cart data async, refactor function names

--- Coding Challenge/cart-calculation.js
+++ Coding Challenge/cart-calculation.js
@@ -1,29 +1,21 @@
-window.onload = () => {
-    const SKUS = {
-        A: { name: "A", price: 60 },
-        B: { name: "B", price: 70 },
-        C: { name: "C", price: 10 },
-        D: { name: "D", price: 20 }
-    };
+window.onload = async () => {
+    const loadCartData = async () => {
+        try {
+            const res = await fetch("cart-data.json");
+            const data = await res.json();
+            return data;
+        } catch (error) {
+            throw Error("Error, cart not loading!")
+        }
+    }
 
-    const shoppingCart = {
-        products: [],
-        quantity: [],
-        price: [],
-        discount: [],
-        subtotal: 0,
-        discountTotal: 0,
-        freeGift: [],
-        totalCost: 0
-    };
+    const calculateOrder = async () => {
+        const data = await loadCartData();
 
-    const cartScenario = {
-        option1: ['A', 'A', 'B', 'C', 'C', 'D'],
-        option2: ['A', 'A', 'A', 'A', 'A', 'B', 'B', 'C', 'D']
-    };
-    const calculateOrder = () => {
+        const { SKUS, shoppingCart, cartScenario } = data;
+
         const newCart = { ...shoppingCart };
-        const currentCart = cartScenario.option1; // Change this to option2 if needed
+        const currentCart = cartScenario.option2; // Change this to option2 if needed
 
         const cartQty = currentCart.reduce((acc, item) => {
             acc[item] = (acc[item] || 0) + 1;
@@ -41,8 +33,8 @@ window.onload = () => {
             newCart.price.push(SKUS[item.name].price);
         });
 
-        const newSubTotal = calculateSubtotal(currentCart);
-        const newDiscountTotal = validateActivePromos(currentCart);
+        const newSubTotal = calculateSubtotal(currentCart, SKUS);
+        const newDiscountTotal = validateActivePromos(currentCart, SKUS);
         const { discountAmount, discount, newFreeGift } = newDiscountTotal;
         const newTotalCost = calculateTotalCost(newSubTotal, discountAmount);
 
@@ -53,23 +45,22 @@ window.onload = () => {
         newCart.freeGift = newFreeGift;
         const finalShoppingCart = { ...shoppingCart, ...newCart }
 
-        showCartItems(finalShoppingCart);
+        showCartItems(finalShoppingCart, SKUS);
 
         const subtotalElem = document.querySelector(".sub-total");
         const discountTotalElem = document.querySelector(".discount-total");
         const orderTotalElem = document.querySelector(".order-total");
 
-        subtotalElem.textContent = `$${shoppingCart.subtotal}`;
-        discountTotalElem.textContent = `$${shoppingCart.discountTotal}`;
-        orderTotalElem.textContent = `$${shoppingCart.totalCost}`;
-
-        console.log(shoppingCart);
+        subtotalElem.textContent = `$${finalShoppingCart.subtotal}`;
+        discountTotalElem.textContent = `$${finalShoppingCart.discountTotal}`;
+        orderTotalElem.textContent = `$${finalShoppingCart.totalCost}`;
     };
 
-    const calculateSubtotal = currentCart =>
-        currentCart.reduce((acc, sku) => acc + SKUS[sku].price, 0);
+    const calculateSubtotal = (currentCart, SKUS) => {
+        return currentCart.reduce((acc, sku) => acc + SKUS[sku].price, 0);
+    }
 
-    const validateActivePromos = currentCart => {
+    const validateActivePromos = (currentCart, SKUS) => {
         let orderPromotions = {
             discountAmount: 0,
             discount: [],
@@ -103,37 +94,40 @@ window.onload = () => {
     const calculateTotalCost = (newSubTotal, discountTotal) =>
         newSubTotal - discountTotal;
 
-    const showCartItems = updatedCart => {
-        const { products, price, quantity, discount, freeGift } = updatedCart;
+    const showCartItems = (updatedCart, SKUS) => {
+        const { products, price, quantity, discount, freeGift, subtotal, totalCost, discountTotal } = updatedCart;
+
+        console.log(updatedCart)
 
         let cartListItems = products.map((sku, index) => `
-    <li class="order-item">
-        <div class="product-info product-name-cont">
-        <div class="product-name">${sku}</div>
-        <div class="product-price">$${price[index]}</div>
-        <div class="product-quantity">${quantity[index]}</div>
-        <div class="product-discount">${discount[index] || ""}</div>
-        </div>
-    </li>
-    `);
-
-        if (freeGift.length > 0) {
-            cartListItems.push(`
         <li class="order-item">
             <div class="product-info product-name-cont">
-            <div class="product-name">${freeGift[0]}</div>
-            <div class="product-price">Free</div>
-            <div class="product-quantity">1</div>
-            <div class="product-discount">$${SKUS[freeGift[0]].price} Value</div>
+            <div class="product-name">${sku}</div>
+            <div class="product-price">$${price[index]}</div>
+            <div class="product-quantity">${quantity[index]}</div>
+            <div class="product-discount">${discount[index] || ""}</div>
             </div>
         </li>
         `);
+
+        if (freeGift.length > 0) {
+            cartListItems.push(`
+            <li class="order-item">
+                <div class="product-info product-name-cont">
+                <div class="product-name">${freeGift[0]}</div>
+                <div class="product-price">Free</div>
+                <div class="product-quantity">1</div>
+                <div class="product-discount">$${SKUS[freeGift[0]].price} Value</div>
+                </div>
+            </li>
+            `);
         }
 
         const cartListItemsTotal = cartListItems.join("");
 
         const cartListCont = document.querySelector(".order-items");
+
         cartListCont.innerHTML = cartListItemsTotal;
     };
-    calculateOrder();
+    await calculateOrder();
 }
