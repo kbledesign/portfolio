820b0c0fc531f876f2d0b4bc33d758409e3e3219
kbledesign <kbledesign@gmail.com>
2023-10-17 19:50:45 -0400

    generator test

---
+++ Javascript App/js/generators.js
@@ -0,0 +1,37 @@
+export default function generators() {
+    function* gen() {
+        let x = 1;
+        x++;
+        console.log("x:", x);
+        yield;
+        x++;
+        console.log("x:", x);
+
+    }
+    const it = gen();
+    it.next();
+    it.next();
+
+    function* genReturn(x, y) {
+        const result = x * 7;
+        return x;
+    }
+
+    const is = genReturn(2, 2);
+    const res = is.next();
+    console.log(res); //returns obj with value
+
+    function* iterationMessage(x) {
+        const y = yield "initiated";
+        const result = x * y;
+        return result;
+    }
+
+    const isit = iterationMessage(6);
+    const res1 = isit.next(5); // starts generator
+    console.log("yield message", res1);
+    const res2 = isit.next(5);
+    console.log("iteration response:", res2);
+
+
+}
