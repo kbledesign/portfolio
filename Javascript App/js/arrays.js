02e34bf1486beafd771d40bf80d08831d80ef42c
kbledesign <kbledesign@gmail.com>
2023-10-20 11:24:57 -0400

    arrays update

--- Javascript App/js/arrays.js
+++ Javascript App/js/arrays.js
@@ -1,16 +1,64 @@
-var a = Array.of(3) // 1, [3]
-var b = Array.of(1, 2, 3) // 3, [1,2,3]
+let a = Array.of(3) // 1, [3]
+let b = Array.of(1, 2, 3) // 3, [1,2,3]
 
-var c = Array.from(arrLike) // [1,2, undefined]
+let z = {
+    length: 3,
+    0: 1,
+    1: 2
+}
 
-var arr = [1, 2, 3, 4, 5];
+let c = Array.from(z) // [1,2, undefined]
+
+let arr = [1, 2, 3, 4, 5];
 arr.copyWithin(3, 0) // mutates
 
-var d = Array(4).fill(undefined); // [undefined...]
-d.fill(41, 1, 3)[undefined, 42, 42, undefined]
+let d = Array(4).fill(undefined); // [undefined...]
+d.fill(41, 1, 3)// [undefined, 42, 42, undefined]
 
-var result = arr.find((v) => v == 2); // 2
-
-var findIndex = arr.findIndex((v) => v === 2) // 2
-
+let result = arr.find((v) => v == 2); // 2
+let findIndex = arr.findIndex((v) => v === 2) // 2
 // values, keys, entries, symbol.iterator
+
+
+let g = Array.from(Array(10), (_, index) => index + 1)
+
+g.push(6); // push to end
+g.pop(); // remove last index
+g.shift() // removes first
+g.unshift(0) // adds to beginning
+g.splice(0, 1, 100)
+let h = g.slice(0, (g.length / 2)); // copies by index/position
+
+g.sort();
+g.reverse();
+
+let i = g.concat(h)
+
+console.log("copyWithin", i);
+i.copyWithin(8, 3, 6)
+console.log("copyWithin", i);
+
+g = g.filter((a) => a % 2 === 0);
+i = g.some((a) => a % 2 > 0);
+h = g.every((a) => a % 2 === 0);
+h = g.includes(9)
+
+g.forEach(fruit => console.log("foreach", fruit * 2));
+
+// h = g.reduce((acc, value, index) => acc + value, 0)
+h = g.reduce((acc, val, index) => {
+    acc[index] = val;
+    return acc;
+}, {})
+
+//g = Array.isArray(g)
+//g = g.lastIndexOf(1);
+//g = g.indexOf(1);
+//g = g.join(",")
+//g = g.toString()
+//g = g.values();
+//g = g.entries();
+//g = g.at(-1)
+
+console.log("Result: g ", g);
+console.log("Result: H", h);
---
+++ Javascript App/js/es6.js
@@ -0,0 +1,101 @@
+console.log("ES6 Features");
+
+{
+    let a = 3; // block scope
+    const b = 4; // read only 
+}
+
+function spread(a, b, c) { }
+spread(...[1, 2, 3])
+
+function rest(a, b, ...c) { }
+rest(1, 2, 3, 4, 5) // 1,2,[3,4,5]
+
+function defaultParameter(a = 11, b = 31) { }
+
+function defaultValueExpressions(a = b + 1, z = (x) => x + 3) { }
+
+//let [e, f, g] = destructure();
+//var { h = 10, i = 20, j = 30 } = bar();
+
+function destructureParam([a, b]) {
+    console.log(a, b);
+}
+
+function defaultParamObj({ x = 10 } = {}, { y } = { y: 10 }) {
+    console.log(x, y);
+}
+
+function foo({ y } = { y: 10 }) { console.log(foo, y) }
+function bar({ y = 10 } = {}) { console.log(bar, y) }
+function car({ y = 10 }) { console.log(car, y) }
+
+// <!--  destructure and restructure 
+var defaults = {
+    theme: 'light',
+    fontSize: 16
+};
+
+var config = {
+    theme: 'dark'
+};
+
+// Merge `defaults` into `config`
+{
+    let {
+        theme = defaults.theme,
+        fontSize = defaults.fontSize
+    } = config;
+
+    // Restructure
+    config = {
+        theme,
+        fontSize
+    };
+}
+// -->
+
+//concise props
+let a = 2, b = 3;
+let c = { a, b }
+
+// concise methods, expressions
+let o = {
+    a() { },
+    b() { }
+}
+// standards for recursion
+let p = {
+    something: function something() { }
+}
+// computed props
+let r = {
+    [prefix + "foo"]: function () { }
+}
+// prototype setting
+let a1 = {};
+let a2 = {
+    __proto__: a1
+}
+
+// super
+const a3 = {
+    foo() { }
+}
+const a4 = {
+    foo() {
+        super.foo()
+    }
+}
+Object.setPrototypeOf(a3, a4);
+//tagged string literals
+
+// Symbols
+const sym = Symbol("key"); // unique value, hidden props, prevent collisions
+sym.toString(); // Symbol("...")
+
+let obj1 = {
+    [sym]: 123
+}
+const sym2 = Symbol.for("key") // returns existing or creates new
+let desc = Symbol.keyFor(sym)
