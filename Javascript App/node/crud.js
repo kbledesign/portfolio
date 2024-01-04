2c9873559094b4628e31962e3e65ba269ea6a4a0
KBLEDEVELOPER <kimball.le.developer@gmail.com>
2024-01-04 09:03:48 -0500

    adding more animals endpoints

--- Javascript App/node/crud.js
+++ Javascript App/node/crud.js
@@ -9,6 +9,8 @@ const { getElementById, getIndexById, updateElement,
 
 const expressions = [];
 seedElements(expressions, 'expressions');
+const animals = [];
+seedElements(animals, 'animals');
 
 const PORT = process.env.PORT || 4001;
 
@@ -44,20 +46,56 @@ app.post('/expressions', (req, res, next) => {
         res.status(400).send();
     }
 });
-
-// Add your DELETE handler below:
 app.delete('/expressions/:id', (req, res, next) => {
-    const id = req.params.id;
-    const expressionIndex = getIndexById(id, expressions);
+    const expressionIndex = getIndexById(req.params.id, expressions);
     if (expressionIndex !== -1) {
         expressions.splice(expressionIndex, 1);
         res.status(204).send();
     } else {
-        res.status(404).send("Invalid id.");
+        res.status(404).send();
     }
-
 });
+app.get('/animals', (req, res, next) => {
+    res.status(200).send(animals);
+})
+
+app.get('/animals/:id', (req, res, next) => {
+    const animal = getElementById(req.params.id, animals)
+    if (animal) {
+        res.send(animal);
+    } else {
+        res.status(404).send();
+    }
+})
+app.put('/animals/:id', (req, res, next) => {
+    const index = getIndexById(req.params.id, animals);
+    if (index !== -1) {
+        updateElement(req.params.id, req.query, animals)
+        res.send(animals[index]);
+    } else {
+        res.status(404).send()
+    }
+})
+app.post('/animals', (req, res, next) => {
+    const receivedAnimal = createElement('animals', req.query);
+    if (receivedAnimal) {
+        animals.push(receivedAnimal)
+        res.status(201).send(receivedAnimal)
+    } else {
+        res.status(400).send();
+    }
+});
+app.delete('/animals', (req, res, next) => {
+    const expressionIndex = getIndexById(req.params.id, expressions);
+    if (expressionIndex !== -1) {
+        animals.splice(expressionIndex, 1);
+        res.status(204).send();
+    } else {
+        res.status(404).send();
+    }
+})
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
 });
+
